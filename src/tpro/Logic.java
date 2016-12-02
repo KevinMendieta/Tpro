ce20e9186daa2cc3f02f28963064a4735830b95a
KevinMendieta <kevin.mendieta-p@mail.escuelaing.edu.co>
2016-12-02 11:47:49 -0500

    fin

--- src/tpro/Logic.java
+++ src/tpro/Logic.java
@@ -11,7 +11,31 @@ package tpro;
  */
 public class Logic {
     
+    private int[] ft;
+    
+    public void fact(int n){
+        ft = new int[n];
+        ft[0] = 1; ft[1] = 1;
+        for (int i = 2; i < ft.length ; i++){
+            ft[i] = i*ft[i-1];
+        }
+    }
+    
+    public int comb(int n, int k){
+        return (ft[n]/((ft[n-k])*ft[k]));
+    }   
+    
     public int calculate(int n){
-        return n;
+        fact(n+1);
+        int[] a = new int[n+1];
+        a[0] = 1;a[1] = 1;int sum;
+        for(int k = 2; k<n+1 ; k++){
+            sum = 0;
+            for(int i = 0; i<k ;i++){
+                sum += a[i]*comb(k,k-i);
+            }
+            a[k]=sum;
+        }        
+        return a[n];
     }
 }
